1911e79675d99ac5357dfba2ce8106074fa43b1e
TCAGM-Dev <thomas@andringa.org>
2026-03-15 09:49:46 +0000

    Add hyprlock

--- stow/.config/rofi/scripts/power.sh
+++ stow/.config/rofi/scripts/power.sh
@@ -7,6 +7,7 @@ if [ $# -gt 0 ]; then
         "Reboot") systemctl reboot ;;
         "Logout") command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit ;;
         "Sleep") systemctl sleep ;;
+        "Lock") coproc (pidof hyprlock || playerctl -a pause && hyprlock >/dev/null 2>&1) ;;
     esac
     exit 0
 fi
@@ -15,3 +16,4 @@ echo -e "Shutdown\n\0icon\x1fshutdown" # TODO: fix icon
 echo -e "Reboot\n\0icon\x1freboot" # TODO: fix icon
 echo -e "Logout\n\0icon\x1flogout" # TODO: fix icon
 echo -e "Sleep\n\0icon\x1fsleep" # TODO: fix icon
+echo -e "Lock\n\0icon\x1flock" # TODO: fix icon
