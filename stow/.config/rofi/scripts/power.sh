d7a6872d2eb59d0a9eaefaf91b449cf2c36988c2
TCAGM-Dev <thomas@andringa.org>
2026-03-16 09:55:19 +0100

    Fully configure suspending/locking

--- stow/.config/rofi/scripts/power.sh
+++ stow/.config/rofi/scripts/power.sh
@@ -7,7 +7,7 @@ if [ $# -gt 0 ]; then
         "Reboot") systemctl reboot ;;
         "Logout") command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit ;;
         "Sleep") systemctl sleep ;;
-        "Lock") coproc (pidof hyprlock || playerctl -a pause && hyprlock >/dev/null 2>&1) ;;
+        "Lock") coproc (playerctl -a pause; pidof hyprlock || hyprlock >/dev/null 2>&1) ;;
     esac
     exit 0
 fi
