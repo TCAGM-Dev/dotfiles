35b9218365bf3b1f8f2548ddf606b3b1b502bba4
TCAGM-Dev <thomas@andringa.org>
2026-03-20 14:20:01 +0100

    Fix Thunar "open with"

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ pacman -S stow
 cd $SCRIPT_DIR/stow && stow --target="$HOME" . # Link dotfiles
 
 pacman -S - < $SCRIPT_DIR/pkglist.txt # Install packages
+
+ln -s /usr/bin/kitty /usr/bin/xdg-terminal-exec # Make Kitty the XDG default terminal (fixes thunar "open with" terminal apps)
