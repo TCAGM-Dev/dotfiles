82cc7b7131fb18ef3f67c94fdf0bab70f6b69d9a
TCAGM-Dev <thomas@andringa.org>
2026-03-21 19:12:52 +0100

    Add sxiv image viewer

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ cd $SCRIPT_DIR/stow && stow --target="$HOME" . # Link dotfiles
 pacman -S - < $SCRIPT_DIR/pkglist.txt # Install packages
 
 ln -s /usr/bin/kitty /usr/bin/xdg-terminal-exec # Make Kitty the XDG default terminal (fixes thunar "open with" terminal apps)
+
+# Mime defaults
+xdg-mime default sxiv.desktop image/jpeg image/png image/bmp
