b356997c0b2803ef0b963c2224a71f6e8632d951
TCAGM-Dev <thomas@andringa.org>
2026-05-04 12:44:42 +0200

    Add quick config editing

--- stow/.config/nvim/init.lua
+++ stow/.config/nvim/init.lua
@@ -91,6 +91,10 @@ vim.api.nvim_create_user_command('GitBlameLine', function()
   print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
 end, { desc = 'Print the git blame for the current line' })
 
+vim.api.nvim_create_user_command("Config", function()
+  vim.cmd("e ~/.config/nvim/init.lua")
+end, { desc = "Open the root config file" })
+
 -- PLUGINS
 --
 -- See `:h :packadd`, `:h vim.pack`
