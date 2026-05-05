d93786b37465b40455eedea700b7f2d74b209150
TCAGM-Dev <thomas@andringa.org>
2026-05-05 15:22:12 +0200

    Move central nvim config to better location

---
+++ stow/.config/nvim/lua/main.lua
@@ -0,0 +1,123 @@
+-- Set <space> as the leader key
+-- See `:h mapleader`
+-- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+
+-- OPTIONS
+--
+-- See `:h vim.o`
+-- NOTE: You can change these options as you wish!
+-- For more options, you can see `:h option-list`
+-- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'`
+-- (Note the single quotes)
+
+vim.o.number = true -- Show line numbers in a column.
+vim.opt.termguicolors = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.wrap = false
+
+-- Show line numbers relative to where the cursor is.
+-- Affects the 'number' option above, see `:h number_relativenumber`.
+vim.o.relativenumber = false
+
+vim.diagnostic.enable()
+
+-- Sync clipboard between OS and Neovim. Schedule the setting after `UIEnter` because it can
+-- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
+-- See `:h 'clipboard'`
+vim.api.nvim_create_autocmd("UIEnter", {
+	callback = function()
+		vim.o.clipboard = "unnamedplus"
+	end,
+})
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.cursorline = true -- Highlight the line where the cursor is on.
+vim.o.scrolloff = 10 -- Keep this many screen lines above/below the cursor.
+vim.o.list = true -- Show <tab> and trailing spaces.
+
+-- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
+vim.o.confirm = true
+
+-- KEYMAPS
+--
+-- See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
+
+-- Use <Esc> to exit terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
+vim.keymap.set({ "t", "i" }, "<A-h>", "<C-\\><C-n><C-w>h")
+vim.keymap.set({ "t", "i" }, "<A-j>", "<C-\\><C-n><C-w>j")
+vim.keymap.set({ "t", "i" }, "<A-k>", "<C-\\><C-n><C-w>k")
+vim.keymap.set({ "t", "i" }, "<A-l>", "<C-\\><C-n><C-w>l")
+vim.keymap.set({ "n" }, "<A-h>", "<C-w>h")
+vim.keymap.set({ "n" }, "<A-j>", "<C-w>j")
+vim.keymap.set({ "n" }, "<A-k>", "<C-w>k")
+vim.keymap.set({ "n" }, "<A-l>", "<C-w>l")
+
+vim.keymap.set({ "n" }, "<S-l>", "<Cmd>bn<CR>")
+vim.keymap.set({ "n" }, "<S-h>", "<Cmd>bp<CR>")
+vim.keymap.set({ "n" }, "<C-c>", "<Cmd>bd<CR>")
+
+-- AUTOCOMMANDS (EVENT HANDLERS)
+--
+-- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
+
+-- Highlight when yanking (copying) text.
+-- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+-- USER COMMANDS: DEFINE CUSTOM COMMANDS
+--
+-- See `:h nvim_create_user_command()` and `:h user-commands`
+
+-- Create a command `:GitBlameLine` that print the git blame for the current line
+vim.api.nvim_create_user_command("GitBlameLine", function()
+	local line_number = vim.fn.line(".") -- Get the current line number. See `:h line()`
+	local filename = vim.api.nvim_buf_get_name(0)
+	print(vim.system({ "git", "blame", "-L", line_number .. ",+1", filename }):wait().stdout)
+end, { desc = "Print the git blame for the current line" })
+
+vim.api.nvim_create_user_command("Config", function()
+	vim.cmd("e ~/.config/nvim/init.lua")
+end, { desc = "Open the root config file" })
+
+vim.api.nvim_create_user_command("Format", function()
+	vim.lsp.buf.format()
+end, { desc = "Format the open buffer using the applicable LSP" })
+
+-- PLUGINS
+--
+-- See `:h :packadd`, `:h vim.pack`
+
+-- Add the "nohlsearch" package to automatically disable search highlighting after
+-- 'updatetime' and when going to insert mode.
+vim.cmd("packadd! nohlsearch")
+
+require("plugins").load({
+	require("plugins/transparent"),
+	require("plugins/gitsigns"),
+	require("plugins/barbar"),
+	require("plugins/log-highlight"),
+	require("plugins/colorizer"),
+	require("plugins/multicursor"),
+	require("plugins/fzf"),
+	require("plugins/mini-completion"),
+	require("plugins/quicker"),
+	require("plugins/lspconfig")({
+		qmlls = { cmd = { "qmlls6" } },
+		ts_ls = {},
+		stylua = { filetypes = { "lua", "luau" } },
+	}),
+})
