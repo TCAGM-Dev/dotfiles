9b517d8b079465a02d687a180c5954f5261782c9
TCAGM-Dev <thomas@andringa.org>
2026-05-05 18:08:04 +0200

    Add nvim fzf keybind

--- stow/.config/nvim/lua/plugins/fzf.lua
+++ stow/.config/nvim/lua/plugins/fzf.lua
@@ -3,5 +3,7 @@ return {
 	dependencies = {"https://github.com/nvim-tree/nvim-web-devicons"},
 	config = function()
 		require("fzf-lua").setup({ fzf_colors = true })
+
+		vim.keymap.set({ "n" }, "<C-o>", "<Cmd>FzfLua files<CR>")
 	end
 }
