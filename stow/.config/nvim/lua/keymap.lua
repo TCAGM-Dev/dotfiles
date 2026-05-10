80e8f67d7aefaf60d78ccd537085059f7582d2ea
TCAGM-Dev <thomas@andringa.org>
2026-05-10 16:53:13 +0200

    Disable conflicting quick move keymaps

--- stow/.config/nvim/lua/keymap.lua
+++ stow/.config/nvim/lua/keymap.lua
@@ -28,7 +28,7 @@ return {bind = function()
 	vim.keymap.set({"n", "i"}, "<CA-Return>", vim.lsp.buf.references)
 
 	vim.keymap.set({"n", "i"}, "<S-Up>", "10-")
-	vim.keymap.set({"n", "i"}, "<S-K>", "10-")
+	vim.keymap.set({"n"}, "<S-K>", "10-")
 	vim.keymap.set({"n", "i"}, "<S-Down>", "10<CR>")
-	vim.keymap.set({"n", "i"}, "<S-J>", "10<CR>")
+	vim.keymap.set({"n"}, "<S-J>", "10<CR>")
 end}
