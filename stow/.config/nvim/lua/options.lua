35b3a5882041b29e5c847f228a59ace35d6bf940
TCAGM-Dev <thomas@andringa.org>
2026-05-31 19:27:02 +0200

    Remove nvim duplicate mode display

--- stow/.config/nvim/lua/options.lua
+++ stow/.config/nvim/lua/options.lua
@@ -16,4 +16,6 @@ return {
 	list = true, -- Show <tab> and trailing spaces.
 
 	confirm = true, -- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`), instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
+
+	showmode = false, -- Hide default mode display, replaced by statusline
 }
