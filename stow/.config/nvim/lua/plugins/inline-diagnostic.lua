5c46ed806176310fa6104b1f8557194d734cbacf
TCAGM-Dev <thomas@andringa.org>
2026-09-22 20:40:07 +0200

    feat(nvim): customize inline diagnostics

--- stow/.config/nvim/lua/plugins/inline-diagnostic.lua
+++ stow/.config/nvim/lua/plugins/inline-diagnostic.lua
@@ -4,6 +4,17 @@ return {
 	config = function()
 		vim.diagnostic.config({virtual_text = false})
 
-		require("tiny-inline-diagnostic").setup() -- TODO: Customize
+		require("tiny-inline-diagnostic").setup({
+			hi = {
+				mixing_color = "StatusLine", -- Using fully transparent `Normal` (default) highlight
+			},
+			signs = {
+				left = "",
+				right = "",
+			},
+			blend = {
+				factor = 0.17,
+			},
+		})
 	end,
 }
